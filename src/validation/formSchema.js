b5a0c0eee36c669211fc5acfa9868b70447d8ecf
Kevin MacFadyen <kmacfadyen3@gmail.com>
2023-01-12 17:38:36 -0500

    completed

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -1,1 +1,25 @@
 // Here goes the schema for the form
+import * as yup from 'yup';
+
+const formSchema = yup.object().shape( {
+  username: yup
+  .string()
+  .trim()
+  .required("username is required!")
+  .min(3, 'username must be 3 characters long!'),
+  email: yup
+  .string()
+  .email('must be a valid email address')
+  .required('you need an email!'),
+  role: yup
+  .string()
+  .oneOf(['instructor', 'student', 'alumni'], 'role is required!'),
+  civil: yup
+  .string()
+  .oneOf(['married', 'single'], 'civil status is required!'),
+  coding: yup.boolean(),
+  reading: yup.boolean(),
+  hiking: yup.boolean()
+})
+
+export default formSchema;
